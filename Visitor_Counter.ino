baa483cee245f42dd8c353f66ab25177e759bd27
Vignesh Rahav S <159713391+vigneshrahav10@users.noreply.github.com>
2024-02-11 23:28:16 +0530

    Add files via upload

---
+++ Visitor_Counter.ino
@@ -0,0 +1,33 @@
+const int ir1=3;
+const int ir2=6;
+int count=0;
+
+void setup() {
+  pinMode(ir1,INPUT);
+  pinMode(ir2,INPUT);
+  Serial.begin(9600);
+  Serial.println("******DIGITAL FOOTFALL TRACKER******");
+}
+
+void loop() {
+  int ir1State=digitalRead(ir1);
+  int ir2State=digitalRead(ir2);
+
+  if (ir1State==LOW && ir2State==HIGH){
+    count+=1;
+    Serial.print("Total:");Serial.println(count);
+    delay(500);
+  }
+  
+  else if(ir1State==HIGH && ir2State==LOW){
+        count-=1;
+    Serial.print("Total:");Serial.println(count);
+    delay(500);
+    if (count==0){
+      Serial.println("******DIGITAL FOOTFALL TRACKER******");
+      delay(10000);
+    }
+  }
+ 
+
+}
